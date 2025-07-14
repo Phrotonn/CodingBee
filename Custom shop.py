1244fb099ec5e02480c6a473004681ebe1b07971
Phrotonn <149646857+Phrotonn@users.noreply.github.com>
2025-07-14 12:31:31 +0700

    Update Custom shop.py

--- Custom shop.py
+++ Custom shop.py
@@ -2,8 +2,11 @@ import os,time,datetime,pytz
 
 a = 0
 b = ""
+c = ""
+d = ""
 i = 1 #list "items" counter1
-total = 0
+Items = " "
+items = " "
 
 data1 = datetime.datetime.now(pytz.timezone("Asia/Jakarta")).strftime("%H;%M;%S")#time 
 
@@ -12,15 +15,51 @@ def Wait():
     input("")
     os.system("cls")
 
+def Checkout():
+    payment = 0
+    total = 0
+    with open(data1 + ".txt", "r") as x:
+        lines = x.readlines()
+
+    for line in lines:
+        if "$" in line:
+            price = int(line.strip().split("$")[1])
+            total += price
+        else:
+            continue
+
+    if d == "cash":
+        print(f"Your total is : ${total}")
+        payment = int(input("Amount : "))
+        if payment < total: 
+            print("Insufficient Gold!")
+            Wait()
+            Open()
+        change = payment - total
+        print(f"Here is your change : ${change}")
+        print("Gratitude upon thee, noble patron, for thy treasured purchase. May fortune and enchantment follow wherever thy journey leads!") 
+        Wait()
+        exit()
+
+    elif d == "card":
+        print("Gratitude upon thee, noble patron, for thy treasured purchase. May fortune and enchantment follow wherever thy journey leads!")  
+        Wait()
+        exit()
+
+    else:
+        print("Please choose between [cash/card]")
+        Wait()
+        Open()
+
 def Open():
     global data1
-    global total
-    global payment
+    global c
+    global d
     while True:
         print(8*"=" + " Online Dungeon Shop " + 8*"=")
         print('''1. Shop
 2. Exit''')
-        1
+        
         if os.path.exists(data1 + ".txt"):
             print("3. Report")
             print(37*"=")
@@ -34,11 +73,11 @@ def Open():
             Wait()
             Shop()
 
-        if a == 2:
+        elif a == 2:
             print("Farewell, adventurer.May the goddess bless upon your journey")
             break
 
-        if a == 3:
+        elif a == 3:
             with open(data1 + ".txt","r") as x:
                 report = x.read()
                 os.system("cls")
@@ -46,34 +85,8 @@ def Open():
                 c = input("Are you ready to cash out? (Y/N) ")
 
             if c == "Y":
-                d = input("Will you pay by cash or card? ")  
-
-                if d == "cash":
-                    total = 0
-                    with open(data1 + ".txt", "r") as x:
-                        lines = x.readlines()
-
-                    for line in lines:
-                        if "$" in line:
-                            price = int(line.strip().split("$")[1])
-                            total += price
-
-                        else :
-                            pass
-                            # break
-                    
-                    print(f"Your total is : ${total}")
-                    payment = int(input("Amount : "))
-                    change = payment - total
-                    print(f"Here is your change : ${change}")
-                    print("Gratitude upon thee, noble patron, for thy treasured purchase. May fortune and enchantment follow wherever thy journey leads!")  
-                    Wait()
-                    break
-
-                if d == "card":
-                    print("Gratitude upon thee, noble patron, for thy treasured purchase. May fortune and enchantment follow wherever thy journey leads!")  
-                    Wait()
-                    break  
+                d = input("Will you pay by cash or card? ")
+                Checkout()
 
             elif c == "N":
                 Wait()
@@ -87,20 +100,16 @@ def Open():
 def Shop():
     global i
     global data1
+    global Items
+
     print(8 * "=" + " Online Dungeon Shop " + 8 * "=")
-    items =[
-        ["Smoke of Deceit",50],
-        ["Sentry Ward",50],
-        ["Observer Ward",75],
-        ["Dust of Appearance",80],
-        ["Healing Salve",100],
-        ["Quelling Blade",100],
-        ["Gem of True Sight",900],
-        ["Shadow Amulet",1000],
-        ["Blink Dagger",2250],
-        ["Moon Shard",4000]
-    ]
-    
+
+    items = [] 
+    with open("items.txt", "r") as file:
+        for line in file:
+            item, price = line.strip().split(",") #["item","price"]
+            items.append([item, int(price)]) #insert items into list 
+
     for item in items:
         print(f"{i}. {item[0]} ${item[1]}")
         i += 1
@@ -108,7 +117,7 @@ def Shop():
             i = 1
 
     print(37 * "=")
-    
+
     b = input("What relic shall aid you on your quest? ")
 
     if b == "":
@@ -116,22 +125,21 @@ def Shop():
         Wait()
         Open()
 
-    elif b.isdigit() and 1 <= int(b) <= 10: #isdigit = to find out if input = int, if input = int then checks if input is 1 - 10
-        selected_item = items[int(b) - 1] #to select item from list of "items"
+    elif b.isdigit() and 1 <= int(b) <= 10:
+        selected_item = items[int(b) - 1]
         print(f"Ah, a fine choice! May this {selected_item} serve you well on your journey. Safe travels, adventurer.")
         Wait()    
-        
-        if not os.path.exists(data1 + ".txt"): #similar to if os.path.exists but this is the opposite, only write when data1.txt doesnt exist. If it exists then it doesnt do anything
-            with open(data1 + ".txt","a") as x:
+
+        if not os.path.exists(data1 + ".txt"):
+            with open(data1 + ".txt", "a") as x:
                 x.write("Your selected item(s) : \n")
-                
-        with open(data1 + ".txt","a") as x:      
-            x.write(f"{selected_item[0]} ${selected_item[1]}" "\n")
+
+        with open(data1 + ".txt", "a") as x:
+            x.write(f"{selected_item[0]} ${selected_item[1]}\n")
 
     else:
         print("Such relic does not exist in our inventory...")
         Wait()
         Open()
-        
-Open()
 
+Open()
